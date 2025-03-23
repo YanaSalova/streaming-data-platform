a091052defd0b7bb022837c3762a3c6837629d5b
YanaSalova <yana.salova.02@gmail.com>
2025-03-23 17:57:17 +0300

    c

--- consumer_to_cassandra.py
+++ consumer_to_cassandra.py
@@ -4,19 +4,17 @@ from cassandra.cluster import Cluster
 from cassandra.query import SimpleStatement
 from datetime import datetime
 
-# Конфигурация Kafka
-KAFKA_BROKER = 'kafka:9092'  # имя контейнера Kafka из docker-compose
+KAFKA_BROKER = 'kafka:9092'  
 TOPIC = 'user_events'
 
-# Конфигурация Cassandra
-CASSANDRA_CONTACT_POINTS = ['cassandra_db']  # имя контейнера Cassandra из docker-compose
+
+CASSANDRA_CONTACT_POINTS = ['cassandra_db'] 
 KEYSPACE = 'mykeyspace'
 TABLE = 'user_events'
 
-# Функция для безопасного десериализатора
+
 def safe_deserialize(x):
     try:
-        # Если x пустой, вернём None
         if not x:
             return None
         return json.loads(x.decode('utf-8'))
@@ -24,7 +22,7 @@ def safe_deserialize(x):
         print("Ошибка при десериализации:", e, flush=True)
         return None
 
-# Создание Kafka consumer с использованием безопасного десериализатора
+
 consumer = KafkaConsumer(
     TOPIC,
     bootstrap_servers=[KAFKA_BROKER],
@@ -32,11 +30,10 @@ consumer = KafkaConsumer(
     value_deserializer=safe_deserialize
 )
 
-# Подключение к Cassandra
 cluster = Cluster(CASSANDRA_CONTACT_POINTS)
 session = cluster.connect(KEYSPACE)
 
-# Подготовка запроса для вставки
+
 insert_query = SimpleStatement(
     f"INSERT INTO {TABLE} (country, event_time, id, name) VALUES (%s, %s, %s, %s)"
 )
@@ -44,7 +41,6 @@ insert_query = SimpleStatement(
 print("Kafka consumer запущен. Чтение сообщений...", flush=True)
 
 for msg in consumer:
-    # Если сообщение не удалось десериализовать или оно пустое, пропускаем его
     if msg.value is None:
         continue
 
